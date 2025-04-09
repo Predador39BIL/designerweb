7fa77778af1bb944b16fd5b8c696a230a69a013e
Predador39BIL <aeromovel.39bil@gmail.com>
2025-04-08 21:31:07 -0300

    upload js, css, html

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// comentário
+
+/* const é o Objeto Icone para trocar a cor do site */
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
+
+function changeTheme () {
+
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
+
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+
+    
+}
+
+    toggleTheme.addEventListener("click", changeTheme);
